fbc39e232089ff492e234bdbba604fb90758d549
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-02 17:50:19 -0300

    test: shoud be created and returned category if is_active, created_at is not provided

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -20,5 +20,25 @@ describe("Category Constructor", () => {
         is_active: true
     });
 
+    it("should be created and returned category, is_active and created_at is not provided", () => {
+
+      let props = {
+          id:'any_id',
+          name: 'any_name',
+          description: 'any_description'
+      }
+  
+      let category = new Category(props);
+  
+      const categoryWithoutCreatedAt = omit(category.props, 'created_at');
+  
+      expect(categoryWithoutCreatedAt).toStrictEqual({
+          id:'any_id',
+          name: 'any_name',
+          description: 'any_description',
+          is_active: true
+      });
   });
+
 })
+});
