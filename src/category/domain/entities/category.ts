b3d4d7abab3189eca5cd24f6ec8904bc4afdf413
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-05 20:44:59 -0300

    feat: create upadate, activate and deactivate method in category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -16,8 +16,29 @@ export class Category extends Entity<props>{
         super(props, id);
         this.props.created_at = this.props.created_at ?? new Date();
         this.props.is_active = this.props.is_active ?? true;
-        this.props.description = this.props.description ?? null;
+        this.description = this.props.description ?? null;
     } 
+
+    update(name: string, description: string): void{
+        this.name = name;
+        this.description = description;
+    }
+
+    activate(): void{
+        this.props.is_active = true;
+    }
+
+    deactivate(): void{
+        this.props.is_active = false;
+    }
+
+    private set name(name: string){
+        this.props.name = name;
+    }
+
+    private set description(description: string){
+        this.props.description = description;
+    }
 
     get name(): string{
         return this.props.name;
