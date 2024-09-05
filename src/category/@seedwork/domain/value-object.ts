2fed07807ccd4dc3fe9b9cde5555ac5cf3978a2f
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-05 15:03:21 -0300

    feat: create value-object abstract class

--- src/category/@seedwork/domain/value-object.ts
+++ src/category/@seedwork/domain/value-object.ts
@@ -1,11 +1,21 @@
-export abstract class ValueObject<T = any>{
+export abstract class ValueObject<T = any> {
     protected _value: T;
 
-    constructor(value: T){
+    constructor(value: T) {
         this._value = value;
     }
 
-    get value(): T{
+    get value(): T {
         return this._value;
+    }
+
+    toString = (): string => {
+        if (this._value === null || this._value === undefined || Number.isNaN(this._value)) {
+            return String(this._value);
+        }
+        if (typeof this._value === 'object') {
+            return JSON.stringify(this._value);
+        }
+        return String(this._value);
     }
 }
