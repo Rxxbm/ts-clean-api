dc739e6b955554e3f754a202ea2e1db6f55cff4b
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-06 11:42:03 -0300

    refactor: add lint in code

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,118 +1,156 @@
 import { Category } from "./category";
-import { omit } from 'lodash';
+import { omit } from "lodash";
 import { validate as uuidValidate } from "uuid";
 
 describe("Category Constructor", () => {
   it("should be created and returned category if description, is_active and created_at is not provided", () => {
-
     let props = {
-        id:'any_id',
-        name: 'any_name'
-    }
+      id: "any_id",
+      name: "any_name",
+    };
 
     let category = new Category(props);
 
-    const categoryWithoutCreatedAt = omit(category.props, 'created_at');
+    const categoryWithoutCreatedAt = omit(category.props, "created_at");
 
     expect(categoryWithoutCreatedAt).toStrictEqual({
-        id:'any_id',
-        name: 'any_name',
-        description: null,
-        is_active: true
+      id: "any_id",
+      name: "any_name",
+      description: null,
+      is_active: true,
     });
   });
-    it("should be created and returned category, is_active and created_at is not provided", () => {
+  it("should be created and returned category, is_active and created_at is not provided", () => {
+    let props = {
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+    };
 
-      let props = {
-          id:'any_id',
-          name: 'any_name',
-          description: 'any_description'
-      }
-  
-      let category = new Category(props);
-  
-      const categoryWithoutCreatedAt = omit(category.props, 'created_at');
-  
-      expect(categoryWithoutCreatedAt).toStrictEqual({
-          id:'any_id',
-          name: 'any_name',
-          description: 'any_description',
-          is_active: true
-      });
+    let category = new Category(props);
+
+    const categoryWithoutCreatedAt = omit(category.props, "created_at");
+
+    expect(categoryWithoutCreatedAt).toStrictEqual({
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+    });
   });
 
   it("should be created and returned category if created_at is not provided", () => {
-
     let props = {
-        id:'any_id',
-        name: 'any_name',
-        description: 'any_description',
-        is_active: true
-    }
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+    };
 
     let category = new Category(props);
 
-    let categoryWithoutCreatedAt = omit(category.props, 'created_at');
+    let categoryWithoutCreatedAt = omit(category.props, "created_at");
 
     expect(categoryWithoutCreatedAt).toStrictEqual({
-        id:'any_id',
-        name: 'any_name',
-        description: 'any_description',
-        is_active: true
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
     });
 
     props.is_active = false;
 
     category = new Category(props);
 
-    categoryWithoutCreatedAt = omit(category.props, 'created_at');
+    categoryWithoutCreatedAt = omit(category.props, "created_at");
 
     expect(categoryWithoutCreatedAt).toStrictEqual({
-      id:'any_id',
-      name: 'any_name',
-      description: 'any_description',
-      is_active: false
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: false,
+    });
   });
-});
 
   it("should be created and returned category if everything is provided", () => {
-
     const created_at = new Date();
 
     let props = {
-        id:'any_id',
-        name: 'any_name',
-        description: 'any_description',
-        is_active: true,
-        created_at
-    }
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+      created_at,
+    };
 
     let category = new Category(props);
 
     expect(category.props).toStrictEqual({
-        id:'any_id',
-        name: 'any_name',
-        description: 'any_description',
-        is_active: true,
-        created_at
+      id: "any_id",
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+      created_at,
     });
-});
+  });
 
-it("should be create id successfully", () => {
+  it("should be create id successfully", () => {
+    let props = [
+      { name: "any_name", id: "d71de38f-620a-49c2-8dc5-2fafa1f7b09e" },
+      { name: "any_name", id: null },
+      { name: "any_name", id: undefined },
+    ];
 
-  let props = [
-      { name: 'any_name', id: 'd71de38f-620a-49c2-8dc5-2fafa1f7b09e' },
-      { name: 'any_name', id: null },
-      { name: 'any_name', id: undefined }
-  ];
-
-  props.forEach(prop => {
+    props.forEach((prop) => {
       let category = new Category(prop);
       expect(category.entityId).not.toBeNull();
       expect(uuidValidate(category.entityId)).toBeTruthy();
+    });
   });
-});
 
+  it("should be update name and description successfully", () => {
+    let props = {
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+    };
 
-  
+    let category = new Category(props);
+
+    category.update("new_name", "new_description");
+
+    expect(category.props).toMatchObject({
+      name: "new_name",
+      description: "new_description",
+      is_active: true,
+    });
+  });
+
+  it("should be activate category successfully", () => {
+    let props = {
+      name: "any_name",
+      description: "any_description",
+      is_active: false,
+    };
+
+    let category = new Category(props);
+
+    category.activate();
+
+    expect(category.props.is_active).toBeTruthy();
+  });
+
+  it("should be deactivate category successfully", () => {
+    let props = {
+      name: "any_name",
+      description: "any_description",
+      is_active: true,
+    };
+
+    let category = new Category(props);
+
+    category.deactivate();
+
+    expect(category.props.is_active).toBeFalsy();
+  });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,58 +1,55 @@
-import { UniqueEntityUUID } from '../../@seedwork/domain/unique-entity-uuid';
-import { Entity } from '../entity/entity';
+import { UniqueEntityUUID } from "../../@seedwork/domain/unique-entity-uuid";
+import { Entity } from "../entity/entity";
 
 type props = {
-    name: string;
-    description?: string;
-    is_active?: boolean;
-    created_at?: Date;
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
 };
 
-export class Category extends Entity<props>{
-    constructor(
-        public readonly props: props,
-        id?: UniqueEntityUUID
-    ){
-        super(props, id);
-        this.props.created_at = this.props.created_at ?? new Date();
-        this.props.is_active = this.props.is_active ?? true;
-        this.description = this.props.description ?? null;
-    } 
+export class Category extends Entity<props> {
+  constructor(public readonly props: props, id?: UniqueEntityUUID) {
+    super(props, id);
+    this.props.created_at = this.props.created_at ?? new Date();
+    this.props.is_active = this.props.is_active ?? true;
+    this.description = this.props.description ?? null;
+  }
 
-    update(name: string, description: string): void{
-        this.name = name;
-        this.description = description;
-    }
+  update(name: string, description: string): void {
+    this.name = name;
+    this.description = description;
+  }
 
-    activate(): void{
-        this.props.is_active = true;
-    }
+  activate(): void {
+    this.props.is_active = true;
+  }
 
-    deactivate(): void{
-        this.props.is_active = false;
-    }
+  deactivate(): void {
+    this.props.is_active = false;
+  }
 
-    private set name(name: string){
-        this.props.name = name;
-    }
+  private set name(name: string) {
+    this.props.name = name;
+  }
 
-    private set description(description: string){
-        this.props.description = description;
-    }
+  private set description(description: string) {
+    this.props.description = description;
+  }
 
-    get name(): string{
-        return this.props.name;
-    }
+  get name(): string {
+    return this.props.name;
+  }
 
-    get description(): string{
-        return this.props.description;
-    }
+  get description(): string {
+    return this.props.description;
+  }
 
-    get is_active(): boolean{
-        return this.props.is_active;
-    }
+  get is_active(): boolean {
+    return this.props.is_active;
+  }
 
-    get created_at(): Date{
-        return this.props.created_at;
-    }
+  get created_at(): Date {
+    return this.props.created_at;
+  }
 }
