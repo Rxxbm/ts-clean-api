581b28ac0c5700c60e1adc45e45f875a3e45e3fe
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-09 17:39:35 -0300

    test: unit test to class validator

--- src/category/@seedwork/validators/class-validator.spec.ts
+++ src/category/@seedwork/validators/class-validator.spec.ts
@@ -1,3 +1,4 @@
+import * as lib from "class-validator";
 import { ValidatorFields } from "./class-validator-fields";
 
 class stubValidator extends ValidatorFields<{ name: string }> {
@@ -11,5 +12,30 @@ describe("Class Validator Tests", () => {
     const validator = new stubValidator();
     expect(validator.errors).toBeNull();
     expect(validator.validatedData).toBeNull();
+  });
+
+  test("should validate data with errors", () => {
+    const spyValidate = jest.spyOn(lib, "validateSync");
+    spyValidate.mockReturnValue([
+      {
+        property: "name",
+        constraints: { isNotEmpty: "name should not be empty" },
+      },
+    ]);
+    const validator = new stubValidator();
+    expect(validator.validate(null)).toBeFalsy();
+    expect(spyValidate).toHaveBeenCalled();
+    expect(validator.validatedData).toBeNull();
+    expect(validator.errors).toEqual({ name: ["name should not be empty"] });
+  });
+
+  test("should validate data without errors", () => {
+    const spyValidate = jest.spyOn(lib, "validateSync");
+    spyValidate.mockReturnValue([]);
+    const validator = new stubValidator();
+    expect(validator.validate("any_name")).toBeTruthy();
+    expect(spyValidate).toHaveBeenCalled();
+    expect(validator.validatedData).toEqual("any_name");
+    expect(validator.errors).toBeNull();
   });
 });
